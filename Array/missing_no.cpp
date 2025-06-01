bf873bdbebefc37ec4a149982d2f23db8b61e0d0
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-01 08:51:21 +0530

    Create missing_no.cpp

---
+++ Array/missing_no.cpp
@@ -0,0 +1,27 @@
+
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v(n+1,-1);
+        for(int i =0;i<nums.size();i++){
+            v[nums[i]] = nums[i];
+        }
+        for(int i =0;i<v.size();i++){
+            if(v[i]==-1)return i;
+        }
+        return 0;
+    }
+-----------------------------------------------------------------------------------------------------------------
+
+
+  Approach-2 by using XOR:
+
+int ans=0;
+        for(int i =1;i<=nums.size();i++){
+          ans= ans^i;
+        }
+        for(int i =0;i<nums.size();i++){
+           ans= ans^nums[i];
+        }
+        return ans;
+    }
+
