f42bdd60b10f9f83a50f6d4b9003971bad593fcd
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-12 18:23:11 +0530

    Create detect cycle in LL.cpp

---
+++ Link List/detect cycle in LL.cpp
@@ -0,0 +1,15 @@
+
+Approach-> Tortoise & hare : O(n)
+
+bool hasCycle(ListNode *head) {
+        ListNode* slow= head, * fast= head;
+
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow= slow->next;
+            fast= fast->next->next;
+            if(slow== fast){
+                return true;
+            }
+        }
+        return false;
+    }
