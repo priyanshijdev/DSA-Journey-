ae4d204e7eac5572cafb261dbc36aa629e278d8f
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-28 22:14:38 +0530

    add optimised approach

--- Strings/Valid_Anagram.cpp
+++ Strings/Valid_Anagram.cpp
@@ -27,6 +27,21 @@ Brute_force Approach: O(nlogn)
 Optimised Approach:
 
   
-
-
-  
+if (str1.length() != str2.length())
+    return false;
+ 
+  int freq[26] = {0};
+  for (int i = 0; i < str1.length(); i++)
+  {
+    freq[str1[i] - 'A']++;
+  }
+  for (int i = 0; i < str2.length(); i++)
+  {
+    freq[str2[i] - 'A']--;
+  }
+  for (int i = 0; i < 26; i++)
+  {
+    if (freq[i] != 0)
+      return false;
+  }
+  return true;
