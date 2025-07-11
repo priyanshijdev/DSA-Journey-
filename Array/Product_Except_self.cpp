c756ea2cab494ca5ef0e8e7c2e7c0d560fdbf288
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-07-11 21:18:16 +0530

    Update Product_Except_self.cpp

--- Array/Product_Except_self.cpp
+++ Array/Product_Except_self.cpp
@@ -12,4 +12,22 @@ Example 1:
 Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
 
-  Code:
+Code:
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> output(nums.size(), 1);
+
+        int left = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;        
+    }
