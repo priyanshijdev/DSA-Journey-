3e609a21fc950a18a7ff18a7383ad5733deefafb
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-01 19:55:53 +0530

    Create ContainerWater.cpp

---
+++ Array/Two_pointer/ContainerWater.cpp
@@ -0,0 +1,20 @@
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, 
+  the max area of water (blue section) the container can contain is 49.
+
+// brute force apporoach :
+        int maxWater = INT_MIN;
+        int n = height.size();
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++){
+                int calWater= (j-i) *min(height[i] ,height[j]);
+                maxWater= max(maxWater, calWater);
+            }
+        }
+        return maxWater;
+
