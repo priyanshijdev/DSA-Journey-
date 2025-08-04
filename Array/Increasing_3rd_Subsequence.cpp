641d7a75124dffcdfa5aa180b41565cadb9b40a7
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-04 16:48:01 +0530

    Update Increasing_3rd_Subsequence.cpp

--- Array/Increasing_3rd_Subsequence.cpp
+++ Array/Increasing_3rd_Subsequence.cpp
@@ -9,4 +9,17 @@ Input: nums = [1,2,3,4,5]
 Output: true
 Explanation: Any triplet where i < j < k is valid.
 
+  O(n3)
+    bool increasingTriplet(vector<int>& nums) {
+        int n= nums.size();
+        for (int i = 0; i < n; i++)
+  for (int j = i + 1; j < n; j++)
+    for (int k = j + 1; k < n; k++)
+      if (nums[i] < nums[j] && nums[j] < nums[k])
+       { return true;}
+
+return false;
+}
+
+
 
