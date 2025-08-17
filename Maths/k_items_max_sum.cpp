02845eb7162f55e091be3bc8e06f48d58d6073ca
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-17 22:48:22 +0530

    Create k_items_max_sum.cpp

---
+++ Maths/k_items_max_sum.cpp
@@ -0,0 +1,17 @@
+lc: 2006
+There is a bag that consists of items, each item has a number 1, 0, or -1 written on it.
+
+You are given four non-negative integers numOnes, numZeros, numNegOnes, and k.
+
+The bag initially contains:
+
+numOnes items with 1s written on them.
+numZeroes items with 0s written on them.
+numNegOnes items with -1s written on them.
+We want to pick exactly k items among the available items. Return the maximum possible sum of numbers written on the items.
+
+
+   int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        
+       return min(k, numOnes) - max(0, k-numOnes - numZeros);
+    }
