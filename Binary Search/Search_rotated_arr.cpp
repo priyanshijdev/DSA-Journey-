d72465cd98a5a60b275891ccd472bd53eee09a67
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-22 17:35:27 +0530

    Create Search_rotated_arr.cpp

---
+++ Binary Search/Search_rotated_arr.cpp
@@ -0,0 +1,31 @@
+LC: 33
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+  // Binary Search
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;        
+    }
+};
