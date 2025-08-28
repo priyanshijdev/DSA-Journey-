427a3ebc6d58fbc5433a05cf00f8f1feb9631caa
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-28 19:19:54 +0530

    Create subarray_sum_k.cpp

---
+++ Array/Subarray/subarray_sum_k.cpp
@@ -0,0 +1,31 @@
+560. Subarray Sum Equals K
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+
+ for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+
