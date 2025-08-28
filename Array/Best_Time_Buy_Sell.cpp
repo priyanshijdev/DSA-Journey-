549a5496bc7d954e4172de2cb68a2aecfb53e1ec
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-28 18:13:10 +0530

    Update Best_Time_Buy_Sell.cpp

--- Array/Best_Time_Buy_Sell.cpp
+++ Array/Best_Time_Buy_Sell.cpp
@@ -2,11 +2,11 @@ Input: prices = [7,1,5,3,6,4]
 Output: 5
 
   int n = prices.size();
-        int profir = INT_MAX, minProfit =0;
+        int profit =0 , minPrice =INT_MAX;
 
         for(int i=0; i<n; i++){
             minPrice = min(minPrice, prices[i]);
-            profit= max( profit,prices[i]-minPrices);
+            profit= max( profit,prices[i]-minPrice);
         }
         return profit;
     }
