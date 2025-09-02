05b01e2daeb89798f69587ad7d3a7f66584b77db
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-09-02 22:28:14 +0530

    Update subarray_sum_k.cpp

--- Array/Subarray/subarray_sum_k.cpp
+++ Array/Subarray/subarray_sum_k.cpp
@@ -28,4 +28,18 @@ Example 2:
     return cnt;
 }
 
+better approach:
+
+  int n= nums.size(), ans=0;
+        for(int i=0; i<n ; i++){
+            int sum=0; 
+            for(int j=i; j<n; j++){
+                sum+= nums[j];
+                if(sum== k){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
 
