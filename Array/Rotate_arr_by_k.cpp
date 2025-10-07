79ff02678abef3830a8b8078a48ec754b65d9ce3
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-10-07 23:26:21 +0530

    Update Rotate_arr_by_k.cpp

--- Array/Rotate_arr_by_k.cpp
+++ Array/Rotate_arr_by_k.cpp
@@ -3,6 +3,15 @@ Brute force -
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
 
+        k %= n;
+        for (int i = 0; i < k; i++) {
+            int last = nums[n - 1];
+            for (int j = n - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = last;
+        }
+-------------------------------------------------------------------
         if(n==0 || k<=0) return ;
         vector<int> temp(n);
         for(int i=0;i<n;i++){
